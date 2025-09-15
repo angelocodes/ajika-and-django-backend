1c44a0db704633648627df49957686d00204426e
Ajika Angelo <ajikangelo@gmail.com>
2025-09-16 01:52:03 +0300

    the trip_tracking app

---
+++ trip/trip/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.core.validators import MaxValueValidator
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+# Create your models here.
+
+#trips & notes (images)
+
+class Trip(models.Model):
+    city = models.CharField(max_length=100)
+    country = models.CharField(max_length=2) # USA -> US
+    start_date = models.DateField(blank=True, null=True)
+    end_date = models.DateField(blank=True, null=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='trips')
+
+    def __str__(self):
+        return f"{self.city}, {self.country}"
+    
+class Note(models.Model):
+    EXCURSIONS = (
+        ('sightseeing', 'Sightseeing'),
+        ('hiking', 'Hiking'),
+        ('beach', 'Beach'),
+        ('cultural', 'Cultural'),
+        ('adventure', 'Adventure'),
+        ('relaxation', 'Relaxation'),
+        ('other', 'Other'),
+    )
+    trip = models.ForeignKey(Trip, on_delete=models.CASCADE, related_name='notes')
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    type = models.CharField(max_length=100, choices=EXCURSIONS, blank=True)
+    img = models.ImageField(upload_to='notes', blank=True, null=True)
+    rating = models.PositiveSmallIntegerField(default=1, validators=[MaxValueValidator(5)])
+
+    def __str__(self):
+        return f"{self.name} - Note for {self.trip.city} on {self.created_at.strftime('%Y-%m-%d')}"
---
+++ trip/trip/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import HomeView, trip_list
+
+urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
+    path('dashboard/', trip_list, name='trip-list'),
+]
